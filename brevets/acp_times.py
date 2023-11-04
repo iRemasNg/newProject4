4a57fea1c4080940ec139b84305caea9fcf73f4e
root <root@Remas>
2023-11-04 09:54:47 +0300

    modified

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -23,37 +23,6 @@ def open_time(control_dist_km: float, brevet_dist_km: int, brevet_start_time: st
     return opening_time.isoformat()
 
 
-# def close_time(control_dist_km: float, brevet_dist_km: int, brevet_start_time: str) -> str:
-#     # If the control distance is less than 60 km, use the French variation of the algorithm.
-#     if control_dist_km < 60:
-#         closing_time = arrow.get(brevet_start_time).shift(hours=control_dist_km / 20 + 1)
-#         return closing_time.isoformat()
-
-#     total_time = 0
-
-#     if control_dist_km == 0:
-#         closing_time = arrow.get(brevet_start_time).shift(hours=1)
-#         return closing_time.isoformat()
-#     elif control_dist_km == 200 and brevet_dist_km == 200:
-#         closing_time = arrow.get(brevet_start_time).shift(hours=13, minutes=30)
-#         return closing_time.isoformat()
-#     elif control_dist_km == 400 and brevet_dist_km == 400:
-#         closing_time = arrow.get(brevet_start_time).shift(hours=27)
-#         return closing_time.isoformat()
-#     else:
-#         for i in range(len(CLOSE)):
-#             if control_dist_km >= CLOSE[i][0]:
-#                 km = control_dist_km - CLOSE[i][0]
-#                 time = km / CLOSE[i-1][1]
-#                 total_time += time
-#                 control_dist_km -= km
-
-#     brevet_start_time = arrow.get(brevet_start_time)
-#     hours = int(total_time)
-#     minutes = round(60 * (total_time - hours))
-#     closing_time = brevet_start_time.shift(hours=hours, minutes=minutes)
-
-#     return closing_time.isoformat()
 
 
 def close_time(control_dist_km: float, brevet_dist_km: int, brevet_start_time: str) -> str:
